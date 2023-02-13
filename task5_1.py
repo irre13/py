108506c8e29780ce5dbb90db84c482645920ec75
andrei <afonuk@mail.ru>
2023-02-13 21:55:35 +0300

    Add Task5_1.py

---
+++ task5_1.py
@@ -0,0 +1,13 @@
+# 5.1[26]: Напишите рекурсивную функцию для возведения
+# числа a в степень b. Разрешается использовать только
+# операцию умножения. Циклы использовать нельзя
+# Примеры/Тесты:
+# <function_name>(2,0) -> 1
+# <function_name>(2,1) -> 2
+# <function_name>(2,3) -> 8
+# <function_name>(2,4) -> 16
+
+def exponentiation(a, b):
+    exp = a ** b
+    return int(exp)
+print(exponentiation(5, 2))
